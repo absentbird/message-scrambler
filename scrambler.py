64555df9d0efd21a5544bb879e7f2fa885741627
Shawn Bird <absent.bird@gmail.com>
2025-02-27 09:04:54 -0800

    mostly working

---
+++ scrambler.py
@@ -0,0 +1,81 @@
+import random
+import sys
+from english_words import get_english_words_set
+from PIL import Image, ImageDraw, ImageFont
+
+wordlist = list(get_english_words_set(['web2'], lower=True))
+GAPMIN = 2
+HITRATE = 0.5
+
+def suitable(characters, word):
+    gaps = 0
+    hits = []
+    index = 0
+    for position, char in enumerate(word):
+        if index >= len(characters):
+            gaps += 1
+            continue
+        tomatch = characters[index]
+        if char == tomatch:
+            hits.append(position)
+            index += 1
+        else:
+            gaps += 1
+    score = len(hits)
+    if score == len(characters):
+        score += GAPMIN
+    ratio = score * 1.0 / len(word)
+    if ratio >= HITRATE and gaps > GAPMIN:
+        return hits
+    return []
+
+def scramble(text):
+    text = text.replace(' ', '').lower()
+    gibberish = ''
+    keys = []
+    while len(text) > 0:
+        word = random.choice(wordlist)
+        fit = suitable(text, word)
+        while len(fit) == 0:
+            word = random.choice(wordlist)
+            fit = suitable(text, word)
+        for key in fit:
+            keys.append(key+len(gibberish))
+        text = text[len(fit):]
+        gibberish += word + ' '
+    return (gibberish[:-1], keys)
+
+text = "example"
+if len(sys.argv) > 1:
+    text = sys.argv[1]
+output = []
+longest = 0
+for word in text.split():
+    output.append(scramble(word))
+print(output)
+
+font_path = 'LiberationMono-Regular.ttf'
+font_size = 20
+font = ImageFont.truetype(font_path, font_size)
+longest = ''
+for word in output:
+    if len(word[0]) > len(longest):
+        longest = word[0]
+width = round(font.getlength(longest) + (2 * font.size))
+height = (len(output) + 2) * font.size
+img = Image.new('RGB', (width, height), color='white')
+d = ImageDraw.Draw(img)
+color1 = (0, 0, 0)
+color2 = (255, 0, 0)
+y = font.size
+for i, word in enumerate(output):
+    text_width = font.getlength(word[0])
+    x = (width - text_width) / 2
+    for j, char in enumerate(word[0].upper()):
+        if j in word[1]:
+            d.text((x, y), char, color2, font=font)
+        else:
+            d.text((x, y), char, color1, font=font)
+        x += font.getlength(char)
+    y += font.size
+img.save('text_image.png')
